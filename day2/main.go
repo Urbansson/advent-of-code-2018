71028fe89da5e2530bf056965cc37361f9b6acea
Theodor Brandt <theodor.brandt@svt.se>
2018-12-02 23:14:34 +0100

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -23,6 +23,29 @@ func main() {
 		}
 	}
 	fmt.Println(twoTimes * treeTimes)
+
+	for p, l1 := range fc {
+		for _, l2 := range fc[p+1:] {
+			// If same skip
+			if l1 == l2 {
+				continue
+			}
+			count := 0
+			index := 0
+			for cp, char1 := range l1 {
+				//char not same at pos cp
+				if []rune(l2)[cp] != char1 {
+					count++
+					index = cp
+				}
+			}
+			if count == 1 {
+				fmt.Println(l1[:index] + l1[index+1:])
+				return
+			}
+		}
+	}
+
 }
 
 func hasOccurrences(row string, occurrences int) bool {
