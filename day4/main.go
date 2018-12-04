bc509f9a52bb5bdf79abb87f36058e48a9c2d9ac
Theodor Brandt <theodor.brandt@svt.se>
2018-12-04 18:26:28 +0100

    day 4 part 1

---
+++ day4/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/urbansson/advent-of-code/util"
+)
+
+var dateLayout = "2006-01-02 15:04"
+
+type log struct {
+	Time    time.Time
+	Message string
+}
+
+type sleep struct {
+	from time.Time
+	to   time.Time
+}
+
+type guard struct {
+	id    string
+	sleep []sleep
+}
+
+func (g guard) totalSleepDuration() time.Duration {
+	var total time.Duration
+	for _, s := range g.sleep {
+		total += s.to.Sub(s.from)
+	}
+	return total
+}
+
+func (g guard) mostCommonSleepMinute() (int, int) {
+	minutes := make(map[int]int, 0)
+	for _, s := range g.sleep {
+		for i := s.from.Minute(); i < s.to.Minute(); i++ {
+			minutes[i] = minutes[i] + 1
+		}
+	}
+
+	var maxKey int
+	var maxMinute int
+	for k, v := range minutes {
+		if v > maxMinute {
+			maxKey = k
+			maxMinute = v
+		}
+	}
+
+	return maxKey, maxMinute
+}
+
+func main() {
+	args := os.Args[1:]
+	file := args[0]
+	fmt.Println("Using input:", file)
+	fc := util.ReadFile(file)
+
+	sortedLogs := parse(fc)
+
+	guards := make(map[string]*guard, 0)
+
+	var from time.Time
+	var guardID string
+	for _, sl := range sortedLogs {
+
+		if strings.HasPrefix(sl.Message, "Guard") {
+			guardID = getGuardID(sl.Message)
+			if _, ok := guards[guardID]; !ok {
+				guards[guardID] = &guard{guardID, make([]sleep, 0)}
+			}
+
+		} else if strings.HasPrefix(sl.Message, "falls asleep") {
+			from = sl.Time
+		} else if strings.HasPrefix(sl.Message, "wakes up") {
+			guard := guards[guardID]
+			guard.sleep = append(guard.sleep, sleep{from, sl.Time})
+		}
+	}
+
+	var max *guard
+	var max2 *guard
+
+	for _, g := range guards {
+		if max == nil {
+			max = g
+		} else if max.totalSleepDuration() < g.totalSleepDuration() {
+			max = g
+		}
+	}
+	fmt.Println(max.id, max.totalSleepDuration().Minutes(), max.mostCommonSleepMinute())
+}
+
+var replacer = strings.NewReplacer("Guard #", "", " begins shift", "")
+
+func getGuardID(message string) string {
+	//Guard #349 begins shift
+	return replacer.Replace(message)
+}
+
+func parse(input []string) []*log {
+	logs := make([]*log, 0, len(input))
+
+	for _, f := range input {
+		p := strings.Split(f, "] ")
+		t, err := time.Parse(dateLayout, p[0][1:])
+		if err != nil {
+			panic(err)
+		}
+		logs = append(logs, &log{t, p[1]})
+	}
+	sort.Slice(logs, func(i, j int) bool { return logs[i].Time.Before(logs[j].Time) })
+	for _, f := range logs {
+		fmt.Println(f.Time, f.Message)
+	}
+
+	return logs
+}
